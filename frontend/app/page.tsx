31e6a8b237738c34a884d81c60ae9929362e3765
Kohei <1lovehamuhamu.15@gmail.com>
2026-01-17 15:28:34 -0800

    tried tailwindUI

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,13 @@
-"use client"; // ← これを追加
-
 export default function Home() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>TegakiRecipe Debug Panel</h1>
-      <p>ここにバックエンドとの通信結果を表示させます。</p>
-      
-      <button onClick={() => alert('ボタンが押されました')}>
-        テストボタン
-      </button>
+    <div>
+    <h1 className="text-3xl font-bold underline">
+       レシピ
+    </h1>
     </div>
-  );
+    
+    // <button className = "bg-sky-500 disabled:hover:bg-sky--700 ...">
+    //   Save changes
+    // </button>
+  )
 }
